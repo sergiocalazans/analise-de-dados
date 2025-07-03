d706e8e7f9f8cb980f5b30c8ee0c98c7849b19f9
Sérgio Calazans <88505877+sergiocalazans@users.noreply.github.com>
2025-07-03 14:22:04 -0300

    Update informacoesGlobais.js

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -1,4 +1,4 @@
-const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
+const url = 'https://sergiocalazans.github.io/api-redes-sociais/data/dados-globais.json'
 
 async function vizualizarInformacoesGlobais() {
     const res = await fetch(url)
